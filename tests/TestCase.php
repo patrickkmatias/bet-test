25b211f050e5cf3b7cc7adf88019ff0bed6aaf79
Patrick M <patrickmatias.silva@hotmail.com>
2024-10-29 04:02:52 -0300

    test: add `assertHasData` inertia test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,16 @@
 
 namespace Tests;
 
+use Inertia\Testing\AssertableInertia as Assert;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function assertHasData(Assert $data)
+    {
+        return $this->assertTrue(
+            count($data->etc()->toArray()['props']) > 0,
+            "Expected data count to be greater than 0"
+        );
+    }
 }
