48a048c104b200b8354c2bb19ab22c0a034df277
Patrick M <patrickmatias.silva@hotmail.com>
2024-10-30 16:46:33 -0300

    refactor: rename route `/dashboard` to `/users`

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/users', function () {
+    return Inertia::render('Users');
+})->middleware(['auth', 'verified'])->name('users');
 
 Route::middleware('auth')->group(function () {
 
