8863aaa6380bc2ff522c80d3f8c0310eae4bfc64
Patrick M <patrickmatias.silva@hotmail.com>
2024-10-31 17:37:48 +0000

    feat: add registered users view

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,28 +1,38 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/Components/ui/card'
+
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+
+defineProps({
+    users: Array
+})
 </script>
 
 <template>
-    <Head title="users" />
+    <Head title="Users" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
-                users
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Registered users
             </h2>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
-                    <div class="p-6 text-gray-900">
-                        You're logged in!
-                    </div>
+                <div class="p-6 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <Card v-for="u in users" >
+                        <CardHeader>
+                            <CardTitle>{{ u.name }}</CardTitle>
+                            <CardDescription>{{ u.email }}</CardDescription>
+                        </CardHeader>
+                    </Card>
                 </div>
             </div>
         </div>
