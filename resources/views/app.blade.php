345e660258a414af57713486f0c7f0f6548eb811
Patrick M <patrickmatias.silva@hotmail.com>
2024-11-04 15:16:01 +0000

    refactor: import material symbols font from root html and not from global css; add delete icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=delete,expand_circle_down" />
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
